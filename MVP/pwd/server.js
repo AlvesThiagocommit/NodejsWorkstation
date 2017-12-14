bae56cb4f4c742f5561961f970a7a6cba0a1804a
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-14 18:50:43 -0200

    Adicionando bootstrap e pagina inicial

--- MVP/pwd/server.js
+++ MVP/pwd/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public' ));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
