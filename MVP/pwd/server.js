05e8b8b5852905ba5fd52a14404ff9254ff836c2
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-14 05:56:25 -0200

    adicionando hbs

--- MVP/pwd/server.js
+++ MVP/pwd/server.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
+const expressHbs = require('express-handlebars');
+
 
 const app = express();
 
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
@@ -13,6 +17,8 @@ app.use(morgan('dev'));
 app.get('/', (req, res, next) => {
   res.json("Im am here");
 });
+
+
 
 app.listen(3030, (err) => {
   if (err) {
