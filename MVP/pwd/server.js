c118a9103ed0716922efa10016b7799c8f3c9000
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-15 23:21:04 -0200

    Ultimos retoques

--- MVP/pwd/server.js
+++ MVP/pwd/server.js
@@ -4,52 +4,62 @@ const morgan = require('morgan');
 const request = require('request');
 const async = require('async');
 const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+
 
 
 const app = express();
 
+//
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public' ));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-
-//3a648573050231ae1cd40ea76324923d-us17
-//
+app.use(session({
+  resave: true,
+  saveUinitialiazed: true,
+  secret: "arashhahahaha",
+  store: new MongoStore({ url: 'mongodb://root:abc123@ds133331.mlab.com:33331/arashnewsletter'})
+}));
+app.use(flash());
 
 app.route('/')
   .get((req, res, next) => {
-    res.render('main/home');
+    res.render('main/home', { message: req.flash('success') });
   })
-  .post((req, res, next) =>{
+  .post((req, res, next) => {
     request({
-      url: 'https://us15.api.mailchimp.com/3.0/lists/695f8a697d/members',
+      url: 'https://us15.api.mailchimp.com/3.0/lists/e21a2d4e84/members',
       method: 'POST',
       headers: {
-        'Authorization': 'randomUser a648573050231ae1cd40ea76324923d-us17',
+        'Authorization': 'randomUser 3336e0cf91b5a9e8bf2c3aa18d71aad3-us15',
         'Content-Type': 'application/json'
       },
       json: {
         'email_address': req.body.email,
         'status': 'subscribed'
       }
-
-    }, function(err, response, body){
+    }, function(err, response, body) {
       if (err) {
-        console.log(err);        
+        console.log(err);
       } else {
-        console.log("Successfully sent");
+        req.flash('success', 'You have submitted your email');
         res.redirect('/');
       }
     });
   });
 
+// Session = memory store, if you want to perserve the data for future use
+// Data Store = mongodb, redis
 
 app.listen(3030, (err) => {
   if (err) {
-    console.log(err);  
+    console.log(err);
   } else {
-    console.log("Running on Port 3030")  
+    console.log("Running on Port 3030");
   }
 });
