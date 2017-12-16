d8b77ad10b6ae4760f77df1e8771191435928562
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-16 01:59:47 -0200

    Configurações pra rodar o servidor.

---
+++ twitter/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', express({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3030, (err) => {
+  if (err)   console.log(err);
+  console.log(`Running on port ${3030}`);
+});
