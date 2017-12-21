cfcbb85dd4d136b7fc848eba8bb79a72002605e2
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-21 19:15:14 -0200

    Primeiro modelo: user .

---
+++ twitter/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  name: String,
+  password: String,
+  photo: String,
+  tweets: [{
+    tweet: { type: Schema.Types.ObjectId, ref: 'Tweet' }
+  }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
