37f1cc1ed7c6d270935f2cc17610cc82a526fdac
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-22 03:02:10 -0200

    Corrigindo mongoose.connect duplicado .

--- twitter/server.js
+++ twitter/server.js
@@ -11,11 +11,6 @@ const app = express();
 mongoose.connect(config.database, function(err) {
   if (err) console.log(err);
   console.log("connected to the database");
-})
-
-mongoose.connect(config.database, function(err) {
-  if (err) console.log(err);
-  console.log("connected to the database");
 });
 
 app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs' }));
