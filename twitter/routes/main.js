c5361f0350e5f39c636820aa383c81c55dc1d404
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-22 02:23:47 -0200

    Nova rota. /create-new-user .

--- twitter/routes/main.js
+++ twitter/routes/main.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('main/landing');
 
 });
+
+router.get('/create-new-user', (req, res, next) => {
+  var user = new User();
+  user.email = "gtav@gmail.com"
+  user.name = "Jack";
+  user.password = "123456";
+  user.save(function(err) {
+    if (err) return next(err);
+    res.json("Successfully created");
+  });
+});
+
 
 module.exports = router;
