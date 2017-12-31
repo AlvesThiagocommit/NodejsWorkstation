632ce3c538ef812d0a46f03132b60727db49fdcf
lobothiago <lobo.alvesthiago@gmail.com>
2017-12-31 17:34:13 -0200

    Metodos para autenticação de usuario. UserSchema.pre UserSchema.methods.gravatar

--- twitter/models/user.js
+++ twitter/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -10,5 +12,27 @@ const UserSchema = new Schema({
     tweet: { type: Schema.Types.ObjectId, ref: 'Tweet' }
   }]
 });
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next()
+  if (user.password) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if  (err) return next(err);
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) return next();
+        user.password = hash;
+        next(err);
+      });
+    });
+  }
+});
+
+UserSchema.methods.gravatar = function(size) {
+  if (!size) size = 200;
+  if (!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
+  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';  
+};
 
 module.exports = mongoose.model('User', UserSchema);
